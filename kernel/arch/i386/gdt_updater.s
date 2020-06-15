5aeb197c7c46510b4901053892888d19c6ca0e3b
LukeRobbins2112 <lukerobbins2112@gmail.com>
2020-06-15 07:21:23 -0400

    Now only disable interrupts around GDT load instead of in boot.S - want to make sure it's not disabled when we setup interrupts

--- kernel/arch/i386/gdt_updater.s
+++ kernel/arch/i386/gdt_updater.s
@@ -6,15 +6,25 @@
 	.type gdt_flush, @function
 
 gdt_flush:
+	# Disable Interrupts
+	cli
+
+	# Load the GDT
 	movl 4(%esp), %eax
 	lgdt (%eax)
 
+	# Re-enable interrupts
+	sti
+
+	# Load Data Segment Registers
 	movw $0x10, %ax
 	movw %ax, %ds
 	movw %ax, %es
 	movw %ax, %fs
 	movw %ax, %gs
 	movw %ax, %ss
+
+	# Longjump to load code register
 	jmp $0x08,$.flush
 .flush:
 	ret
