0a66fce77fed701257a0a0709150508272306b8e
LukeRobbins2112 <lukerobbins2112@gmail.com>
2020-07-01 10:18:25 -0400

    Modified boot to identity map the entire first 4MB
    Did this to open up more space for things like pmm_frames, k_workspace_end, etc.
    Adjusted VGA address, because now it's automatically mapped; don't need to include special case

--- kernel/arch/i386/tty.c
+++ kernel/arch/i386/tty.c
@@ -9,7 +9,7 @@
  
 static const size_t VGA_WIDTH = 80;
 static const size_t VGA_HEIGHT = 25;
-static uint16_t* const VGA_MEMORY = (uint16_t*) 0xC03FF000;
+static uint16_t* const VGA_MEMORY = (uint16_t*) 0xC00B8000;
  
 static size_t terminal_row;
 static size_t terminal_column;
@@ -20,7 +20,7 @@ void terminal_initialize(void) {
 	terminal_row = 0;
 	terminal_column = 0;
 	terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
-	terminal_buffer = (uint16_t*) 0xC03FF000; // VGA_MEMORY;
+	terminal_buffer = (uint16_t*) 0xC00B8000; // VGA_MEMORY;
 	for (size_t y = 0; y < VGA_HEIGHT; y++) {
 		for (size_t x = 0; x < VGA_WIDTH; x++) {
 			const size_t index = y * VGA_WIDTH + x;
