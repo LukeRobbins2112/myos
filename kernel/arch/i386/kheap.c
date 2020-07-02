2ee68fa8e0f1a1cffbbccf47be45b4489aba0f3c
LukeRobbins2112 <lukerobbins2112@gmail.com>
2020-07-02 09:05:27 -0400

    Implemented kheap basic setup functions setup_kheap and create_heap. Successfully tested; first call page faults and allocs page in desired heap start address

--- kernel/arch/i386/kheap.c
+++ kernel/arch/i386/kheap.c
@@ -1,24 +1,40 @@
-#include "kernel/kheap.h"
+#include <kernel/kheap.h>
+#include <kernel/pmm.h>
+#include <kernel/vmm.h>
+#include <kernel/boot_heap.h>
 
-// ------------------
-// _kernel_end defined in linker
-extern uint32_t _kernel_end;
-static uint32_t next_alloc = (void*)&_kernel_end;
+heap_t* kheap = (heap_t*)0x0;
 
+void setup_kheap(){
 
-uint32_t boot_alloc(uint32_t size, uint32_t align){
+  // Need boot heap to place heap structures
+  setup_boot_heap();
+  kheap = create_heap(0xD0000000, KHEAP_INITIAL_SIZE, 0x3);
 
-  if (align && (next_alloc & 0x00000FFF)){
-    next_alloc += 0xFFF;
-    next_alloc &= 0xFFFFF000;
+}
+
+heap_t* create_heap(uint32_t start_addr, uint32_t size, uint8_t flags){
+
+  heap_t* new_heap = (heap_t*)boot_alloc(sizeof(heap_t), 0);
+  
+  // Make sure start address is page aligned
+  if (start_addr & 0x00000FFF){
+    start_addr += 0x00000FFF;
+    start_addr &= 0xFFFFF000;
   }
 
-  uint32_t new_addr = next_alloc;
-  next_alloc += size;
-  return new_addr;
+  new_heap->heap_start = start_addr;
+  new_heap->prog_break = start_addr;
+  new_heap->heap_end = start_addr + size;
+  new_heap->max_size = 0x1000000;
+  new_heap->flags = flags;
+
+  
+  return new_heap;
 }
 
-uint32_t boot_alloc_frame(){
-  return boot_alloc(0x1000, 1);
+void* kalloc(uint32_t size, uint16_t align, heap_t* heap){
+  void* res = (void*)heap->prog_break;
+  heap->prog_break += size;
+  return res;
 }
-
--- kernel/include/kernel/kheap.h
+++ kernel/include/kernel/kheap.h
@@ -1,20 +1,60 @@
+// **************************************************************
+// **************************************************************
+// Module for managing kernel heap, dynamic memory allocation
+// **************************************************************
+// **************************************************************
+
 #ifndef _KHEAP_H
 #define _KHEAP_H
 
+
 #include <stdint.h>
 
-// ----------------------------------
-// Module for Kernel Heap Operations
-// ----------------------------------
+// --------------------------------------------------------------
+// Macros and Constant Definitions
+// --------------------------------------------------------------
+
+#define KHEAP_START          0xD0000000
+#define KHEAP_INITIAL_SIZE   0x1000
+#define KHEAP_MAGIC          0xFACEB00C
 
 
+// -------------------------------------------------------------
+// Data structure definitions
+// -------------------------------------------------------------
 
-// ----------------------------------
-// Bootstrap Allocator
-// Used before heap is set up
-// ----------------------------------
+typedef struct header {
+  uint32_t size;
+  uint32_t magic;
+} header_t;
 
-uint32_t boot_alloc(uint32_t size, uint32_t align);
-uint32_t boot_alloc_frame();
+typedef struct footer {
+  header_t* header;
+  uint32_t magic;
+} footer_t;
+
+typedef struct heap {
+  uint32_t heap_start; // Start of the heap
+  uint32_t prog_break; // Current end of alloc'd memory
+  uint32_t heap_end;   // End of available heap space; can be expanded
+  uint32_t max_size;   // Heap cannot grow larger than this
+  uint8_t flags;       // 0-bit = kernel, 1-bit = read/write
+} heap_t;
+
+// ------------------------------------------------------------
+// Heap Function Declarations
+// ------------------------------------------------------------
+
+void setup_kheap();
+heap_t* create_heap(uint32_t start_addr, uint32_t size, uint8_t flags);
+void* kalloc(uint32_t size, uint16_t align, heap_t* heap);
+void kfree(void* ptr, heap_t* heap);
+
+// --------------------------------------------------------------
+// External variables
+// --------------------------------------------------------------
+
+extern heap_t* kheap;
+
 
 #endif // _KHEAP_H
